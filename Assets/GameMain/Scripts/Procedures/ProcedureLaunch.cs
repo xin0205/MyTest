8bb4f472471203d1c17a131858a0215dffd09201
xin0205 <397952780@qq.com>
2020-03-17 06:48:54 +0800

    可运行GameFramework的最简代码

---
+++ Assets/GameMain/Scripts/Procedures/ProcedureLaunch.cs
@@ -0,0 +1,42 @@
+﻿//------------------------------------------------------------
+// Game Framework
+// Copyright © 2013-2020 Jiang Yin. All rights reserved.
+// Homepage: https://gameframework.cn/
+// Feedback: mailto:ellan@gameframework.cn
+//------------------------------------------------------------
+
+using GameFramework.Localization;
+using System;
+using UnityEngine;
+using UnityGameFramework.Runtime;
+using ProcedureOwner = GameFramework.Fsm.IFsm<GameFramework.Procedure.IProcedureManager>;
+
+namespace Project
+{
+    public class ProcedureLaunch : ProcedureBase
+    {
+        public override bool UseNativeDialog
+        {
+            get
+            {
+                return true;
+            }
+        }
+
+        protected override void OnEnter(ProcedureOwner procedureOwner)
+        {
+            base.OnEnter(procedureOwner);
+
+            Log.Debug("ProcedureLaunch OnEnter");
+
+            DRTest test = GameEntry.DataTable.GetDataRow<DRTest>(1);
+           
+        }
+
+        protected override void OnUpdate(ProcedureOwner procedureOwner, float elapseSeconds, float realElapseSeconds)
+        {
+            base.OnUpdate(procedureOwner, elapseSeconds, realElapseSeconds);
+
+        }
+    }
+}
