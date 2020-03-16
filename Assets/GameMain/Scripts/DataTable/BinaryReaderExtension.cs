a8a84e29e1cb31368fafb2c1b05a943ca3f607e2
xin0205 <397952780@qq.com>
2020-03-17 06:49:50 +0800

    运行DataTable

---
+++ Assets/GameMain/Scripts/DataTable/BinaryReaderExtension.cs
@@ -0,0 +1,104 @@
+﻿//------------------------------------------------------------
+// Game Framework
+// Copyright © 2013-2020 Jiang Yin. All rights reserved.
+// Homepage: https://gameframework.cn/
+// Feedback: mailto:ellan@gameframework.cn
+//------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+namespace Project
+{
+    public static class BinaryReaderExtension
+    {
+        public static Color32 ReadColor32(this BinaryReader binaryReader)
+        {
+            return new Color32(binaryReader.ReadByte(), binaryReader.ReadByte(), binaryReader.ReadByte(), binaryReader.ReadByte());
+        }
+
+        public static Color ReadColor(this BinaryReader binaryReader)
+        {
+            return new Color(binaryReader.ReadSingle(), binaryReader.ReadSingle(), binaryReader.ReadSingle(), binaryReader.ReadSingle());
+        }
+
+        public static DateTime ReadDateTime(this BinaryReader binaryReader)
+        {
+            return new DateTime(binaryReader.ReadInt64());
+        }
+
+        public static Quaternion ReadQuaternion(this BinaryReader binaryReader)
+        {
+            return new Quaternion(binaryReader.ReadSingle(), binaryReader.ReadSingle(), binaryReader.ReadSingle(), binaryReader.ReadSingle());
+        }
+
+        public static Rect ReadRect(this BinaryReader binaryReader)
+        {
+            return new Rect(binaryReader.ReadSingle(), binaryReader.ReadSingle(), binaryReader.ReadSingle(), binaryReader.ReadSingle());
+        }
+
+        public static Vector2 ReadVector2(this BinaryReader binaryReader)
+        {
+            return new Vector2(binaryReader.ReadSingle(), binaryReader.ReadSingle());
+        }
+
+        public static Vector3 ReadVector3(this BinaryReader binaryReader)
+        {
+            return new Vector3(binaryReader.ReadSingle(), binaryReader.ReadSingle(), binaryReader.ReadSingle());
+        }
+
+        public static Vector4 ReadVector4(this BinaryReader binaryReader)
+        {
+            return new Vector4(binaryReader.ReadSingle(), binaryReader.ReadSingle(), binaryReader.ReadSingle(), binaryReader.ReadSingle());
+        }
+
+        public static List<T> ReadList<T>(this BinaryReader binaryReader, Func<T> parse)
+        {
+            List<T> valueList = new List<T>();
+
+            int count = binaryReader.ReadInt32();
+
+            for (int i = 0; i < count; i++) {
+
+                valueList.Add(parse());
+            }
+
+            return valueList;
+
+
+        }
+
+        //public static List<T> ReadEnumList<T>(this BinaryReader binaryReader) where T: Enum
+        //{
+
+        //    List<int> valueList = new List<int>();
+
+        //    int count = binaryReader.ReadInt32();
+
+        //    for (int i = 0; i < count; i++)
+        //    {
+
+        //        valueList.Add(binaryReader.ReadInt32());
+        //    }
+
+        //    return (List<T>)valueList;
+
+        //}
+
+
+        public static T ReadEnum<T>(this BinaryReader binaryReader) where T : Enum
+        {
+            //T enumInstance;
+            //Enum.TryParse<T>(binaryReader.ReadInt32() + "", out enumInstance);
+
+            //return enumInstance;
+
+            return (T)Enum.Parse(typeof(T), binaryReader.ReadInt32() + "");
+        }
+
+
+    }
+
+}
---
+++ Assets/GameMain/Scripts/DataTable/DRTest.cs
@@ -0,0 +1,127 @@
+﻿//------------------------------------------------------------
+// Game Framework
+// Copyright © 2013-2019 Jiang Yin. All rights reserved.
+// Homepage: http://gameframework.cn/
+// Feedback: mailto:jiangyin@gameframework.cn
+//------------------------------------------------------------
+// 此文件由工具自动生成，请勿直接修改。
+// 生成时间：2020-03-15 20:20:13.763
+//------------------------------------------------------------
+
+using GameFramework;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using UnityEngine;
+using UnityGameFramework.Runtime;
+
+namespace Project
+{
+    /// <summary>
+    /// 测试表格生成。
+    /// </summary>
+    public class DRTest : DataRowBase
+    {
+        private int m_Id = 0;
+
+        /// <summary>
+        /// 获取编号。
+        /// </summary>
+        public override int Id
+        {
+            get
+            {
+                return m_Id;
+            }
+        }
+
+        /// <summary>
+        /// 获取Int值。
+        /// </summary>
+        public int IntValue
+        {
+            get;
+            private set;
+        }
+
+        /// <summary>
+        /// 获取int数组。
+        /// </summary>
+        public List<int> IntList
+        {
+            get;
+            private set;
+        }
+
+        /// <summary>
+        /// 获取Vector2。
+        /// </summary>
+        public Vector2 Vector2Value
+        {
+            get;
+            private set;
+        }
+
+        /// <summary>
+        /// 获取Vector2数组。
+        /// </summary>
+        public List<Vector2> Vector2List
+        {
+            get;
+            private set;
+        }
+
+        public override bool ParseDataRow(GameFrameworkSegment<string> dataRowSegment)
+        {
+            // Star Force 示例代码，正式项目使用时请调整此处的生成代码，以处理 GCAlloc 问题！
+            string[] columnTexts = dataRowSegment.Source.Substring(dataRowSegment.Offset, dataRowSegment.Length).Split(DataTableExtension.DataSplitSeparators);
+            for (int i = 0; i < columnTexts.Length; i++)
+            {
+                columnTexts[i] = columnTexts[i].Trim(DataTableExtension.DataTrimSeparators);
+            }
+
+            int index = 0;
+            index++;
+            m_Id = int.Parse(columnTexts[index++]);
+            index++;
+            IntValue = int.Parse(columnTexts[index++]);
+            IntList = DataTableExtension.ParseList<int>(columnTexts[index++], int.Parse);
+            Vector2Value = DataTableExtension.ParseVector2(columnTexts[index++]);
+            Vector2List = DataTableExtension.ParseList<Vector2>(columnTexts[index++], DataTableExtension.ParseVector2);
+
+            GeneratePropertyArray();
+            return true;
+        }
+
+        public override bool ParseDataRow(GameFrameworkSegment<byte[]> dataRowSegment)
+        {
+            // Star Force 示例代码，正式项目使用时请调整此处的生成代码，以处理 GCAlloc 问题！
+            using (MemoryStream memoryStream = new MemoryStream(dataRowSegment.Source, dataRowSegment.Offset, dataRowSegment.Length, false))
+            {
+                using (BinaryReader binaryReader = new BinaryReader(memoryStream, Encoding.UTF8))
+                {
+                    m_Id = binaryReader.ReadInt32();
+                    IntValue = binaryReader.ReadInt32();
+                    IntList = binaryReader.ReadList<int>(binaryReader.ReadInt32);
+                    Vector2Value = binaryReader.ReadVector2();
+                    Vector2List = binaryReader.ReadList<Vector2>(binaryReader.ReadVector2);
+                }
+            }
+
+            GeneratePropertyArray();
+            return true;
+        }
+
+        public override bool ParseDataRow(GameFrameworkSegment<Stream> dataRowSegment)
+        {
+            Log.Warning("Not implemented ParseDataRow(GameFrameworkSegment<Stream>)");
+            return false;
+        }
+
+        private void GeneratePropertyArray()
+        {
+
+        }
+    }
+}
